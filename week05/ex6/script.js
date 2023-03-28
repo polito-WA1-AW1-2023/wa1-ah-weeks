a84b3abd15c1c2107421d736b028828c5a0eda12
Fulvio Corno <fulvio.corno@polito.it>
2023-03-28 12:57:14 +0200

    ex6 partial solution

---
+++ week05/ex6/script.js
@@ -0,0 +1,74 @@
+'use strict' ;
+
+// SOlution for Exercise 6
+
+// FAKE DATA
+const myquestion = new Question('Is JavaScript better than Python?', 'Luigi De Russis', '2023-01-01') ;
+myquestion.add(new Answer('Yes', 'Luca Mannella', -10, '2023-02-15')) ;
+myquestion.add(new Answer('Both have their pros and cons', 'Mario R0ssi', 0, '2023-03-04')) ;
+
+// console.log(myquestion) ;
+
+document.addEventListener('DOMContentLoaded', (event)=> {
+
+    document.querySelector('p.lead').innerText = myquestion.text ;
+    document.getElementById('questionauthor').innerText = myquestion.author ;
+
+    const table = document.getElementById('answerstable') ;
+    // const tableBody = table.childNodes[1] ;
+    const tableBody = table.querySelector('tbody') ;
+
+    for(const answer of myquestion.answers) {
+        const tr = document.createElement('tr') ;
+        // tr.innerHTML = `<td>${answer.date}</td><td>${answer.text}</td>` ;
+
+        const tdDate = document.createElement('td') ;
+        tdDate.innerText = answer.date.format('MM/DD/YYYY') ;
+        tr.appendChild(tdDate) ;
+
+        const tdText = document.createElement('td') ;
+        tdText.innerText = answer.text ;
+        tr.appendChild(tdText) ;
+
+        const tdAuthor = document.createElement('td') ;
+        tdAuthor.innerText = answer.author ;
+        tr.appendChild(tdAuthor) ;
+
+        const tdScore = document.createElement('td') ;
+        tdScore.innerText = answer.score ;
+        tr.appendChild(tdScore) ;
+
+        const tdButton = document.createElement('td') ;
+        const voteButton = document.createElement('button') ;
+        voteButton.innerText = 'VOTE' ;
+        voteButton.classList.add('btn', 'btn-info');
+        tdButton.appendChild(voteButton);
+        tr.appendChild(tdButton) ;
+
+        voteButton.addEventListener('click', (event)=>{
+            // const oldScore = Number(tdScore.innerText) ;
+            // const newScore = oldScore+1 ;
+            // tdScore.innerText = newScore ;
+
+            const theButton = event.target ;
+            const scoreCell = theButton.parentNode.parentNode.childNodes[3] ;
+            scoreCell.innerText = Number(scoreCell.innerText)+1 ;
+
+        }) ;
+
+        // const tdButton = document.createElement('td') ;
+        // tdButton.innerHTML = `Press here: <button class="btn btn-info">VOTE</button>` ;
+        // tr.appendChild(tdButton) ;
+
+        tableBody.appendChild(tr) ;
+    }
+
+    document.querySelector('#addbutton').addEventListener('click', (event)=>{
+        const date = document.querySelector('input[name="date"]').value ;
+        const text = document.querySelector('input[name="text"]').value ;
+        const author = document.querySelector('input[name="author"]').value ;
+
+        console.log(date, text, author);
+    });
+
+});
