6c53b99e414a37490b1d0481fd168db74ce3e4a7
Fulvio Corno <fulvio.corno@polito.it>
2023-05-30 23:42:12 +0200

    code cleanup and documentation

--- week13/server/user-dao.js
+++ week13/server/user-dao.js
@@ -2,47 +2,41 @@ const crypto = require('crypto');
 
 const db = require('./db');
 
-
 /**
- * Query the database and check whether the usernamen exists and the password
+ * Query the database and check whether the username exists and the password
  * hashes to the correct value.
  * If so, return an object with full user information.
- * @param {*} username 
- * @param {*} password 
+ * @param {string} username 
+ * @param {string} password 
+ * @returns {Promise} a Promise that resolves to the full information about the current user, if the password matches
+ * @throws the Promise rejects if any errors are encountered
  */
 function getUser(username, password) {
     const sql = 'SELECT * FROM user WHERE email=?';
     return new Promise((resolve, reject) => {
-        console.log(db);
         db.get(sql, [username], (err, row) => {
-            if (err) {
+            if (err) { // database error
                 reject(err);
             } else {
-                console.log(row);
-                if (!row) {
+                if (!row) { // non-existent user
                     reject('Invalid username or password');
                 } else {
                     crypto.scrypt(password, row.salt, 32, (err, computed_hash) => {
-                        console.log(computed_hash);
-                        if (err) {
+                        if (err) { // key derivation fails
                             reject(err);
                         } else {
                             const equal = crypto.timingSafeEqual(computed_hash, Buffer.from(row.password, 'hex'));
-                            if (equal) {
+                            if (equal) { // password ok
                                 resolve(row);
-                            } else {
+                            } else { // password doesn't match
                                 reject('Invalid username or password');
                             }
                         }
                     });
-
                 }
             }
         });
     });
-
-
-
 }
 
 exports.getUser = getUser;
